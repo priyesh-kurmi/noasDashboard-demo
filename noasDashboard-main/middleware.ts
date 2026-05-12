c4eb66c8eb7bd394fbffe0b7b2d33def10f58582
Priyesh <kpriyesh1908@gmail.com>
2026-05-12 09:26:32 +0530

    refactor: remove duplicate config export in middleware

--- noasDashboard-main/middleware.ts
+++ noasDashboard-main/middleware.ts
@@ -21,7 +21,3 @@ export function middleware(request: NextRequest) {
 export const config = {
   matcher: ['/((?!_next/static|_next/image|favicon.ico).*)'],
 };
-
-export const config = {
-  matcher: ['/((?!_next/static|_next/image|favicon.ico).*)'],
-};
